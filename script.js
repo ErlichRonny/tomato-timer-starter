3b592ba6e6a74847861653eb2b0d3e39a174b4f4
Ronny Erlich <ronnye@meta.com>
2025-06-04 11:43:53 -0700

    Step 3

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function updateTimerDisplay(minutes, seconds){
-    const timeLeft = document.getElementById('time-left');
-    timeLeft.textContent = `${minutes}:${seconds}`;
+function updateTimerDisplay(minutes, seconds) {
+  const timeLeft = document.getElementById("time-left");
+  timeLeft.textContent = `${minutes}:${seconds}`;
 }
 
-updateTimerDisplay(20, 17)
+
+function startTimer(duration) {
+  let minutes = Math.floor(duration / 60);
+  let seconds = duration % 60;
+  updateTimerDisplay(minutes, seconds);
+
+  const countdown = setInterval(() => {
+    duration--;
+    minutes = Math.floor(duration / 60);
+    seconds = duration % 60;
+    updateTimerDisplay(minutes, seconds);
+  }, 1000);
+
+  setTimeout(() => {
+    clearTimeout(countdown);
+  }, duration * 1000);
+}
+
+startTimer(20);
